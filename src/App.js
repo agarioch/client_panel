f307429515b32f63683e0a3b3111dd63679bf2d2
alistair garioch <alistairgarioch@gmail.com>
2020-04-14 21:24:15 +0100

    Added Add client page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import store from './store';
 
 import AppNavbar from './components/layout/AppNavbar';
 import Dashboard from './components/layout/Dashboard';
+import AddClient from './components/clients/AddClient';
 
 import './App.css';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
@@ -36,7 +37,7 @@ function App() {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Dashboard} />
-
+                <Route exact path="/client/add" component={AddClient} />
               </Switch>
             </div>
           </div>
