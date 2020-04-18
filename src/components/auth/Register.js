b0eb2ff9fdc8930cf11f0547ae06252e1c6e6a77
alistair garioch <alistairgarioch@gmail.com>
2020-04-18 17:47:18 +0100

    add / disable user registration via settings

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -12,18 +12,22 @@ class Login extends Component {
         password: ''
     }
 
+    componentWillMount() {
+        const { allowRegistration } = this.props.settings;
+
+        if (!allowRegistration) {
+            this.props.history.push('/')
+        }
+    }
+
     onSubmit = e => {
         e.preventDefault();
 
         const { firebase, notifyUser } = this.props;
         const { email, password } = this.state;
 
-        firebase
-            .login({
-                email,
-                password
-            })
-            .catch(err => notifyUser('Invalid login details', 'error'));
+        //Register with firebase
+        firebase.createUser({ email, password }).catch(err => notifyUser('That user already exists', 'error'))
     };
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
@@ -37,7 +41,7 @@ class Login extends Component {
                         <div className="card-body">
                             {message ? (<Alert message={message} messageType={messageType} />) : null}
                             <h1 className="text-center pb-4 pt-3">
-                                <span className='text-primary'><i className="fas fa-lock"></i> Login</span>
+                                <span className='text-primary'><i className="fas fa-lock"></i> Register</span>
                             </h1>
                             <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
@@ -58,7 +62,7 @@ class Login extends Component {
                                         value={this.state.password}
                                         onChange={this.onChange} />
                                 </div>
-                                <input type="submit" value="Login" className="btn btn-primary btn-block" />
+                                <input type="submit" value="Register" className="btn btn-primary btn-block" />
                             </form>
                         </div>
                     </div>
@@ -77,6 +81,7 @@ Login.propTypes = {
 export default compose(
     firebaseConnect(),
     connect((state, props) => ({
-        notify: state.notify
+        notify: state.notify,
+        settings: state.settings
     }), { notifyUser })
 )(Login);
